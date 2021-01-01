b3b96670dfd1e09fbda69f90c7fd0cd113740908
abhishekpareek131 <55628288+abhishekpareek131@users.noreply.github.com>
2021-01-01 18:44:06 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,25 +2,25 @@ import threading
 from threading import *
 import time
 
-d={} # Storing the data
+d={} 
 
 
 def create(key,value,timeout=0):
     if key in d:
-        print("error: this key already exists") #error message1
+        print("error: this key already exists") 
     else:
         if(key.isalpha()):
-            if len(d)<(1024*1020*1024) and value<=(16*1024*1024): #constraints for file size less than 1GB and Jasonobject value less than 16KB 
+            if len(d)<(1024*1020*1024) and value<=(16*1024*1024):
                 if timeout==0:
                     l=[value,timeout]
                 else:
                     l=[value,time.time()+timeout]
-                if len(key)<=32: #constraints for input key_name capped at 32chars
+                if len(key)<=32: 
                     d[key]=l
             else:
-                print("error: Memory limit exceeded!! ")#error message2
+                print("error: Memory limit exceeded!! ")
         else:
-            print("error: Invalind key_name!! key_name must contain only alphabets and no special characters or numbers")#error message3
+            print("error: Invalid key_name!! key_name must contain only alphabets and no special characters or numbers")#error message3
 
 
             
@@ -30,8 +30,8 @@ def read(key):
     else:
         b=d[key]
         if b[1]!=0:
-            if time.time()<b[1]: #comparing the present time with expiry time
-                stri=str(key)+":"+str(b[0]) #to return the value in the format of JasonObject i.e.,"key_name:value"
+            if time.time()<b[1]: 
+                stri=str(key)+":"+str(b[0])
                 return stri
             else:
                 print("error: time-to-live of",key,"has expired") 
@@ -46,7 +46,7 @@ def delete(key):
     else:
         b=d[key]
         if b[1]!=0:
-            if time.time()<b[1]: #comparing the current time with expiry time
+            if time.time()<b[1]:
                 del d[key]
                 print("key is successfully deleted")
             else:
